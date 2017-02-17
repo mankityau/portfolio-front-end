3dfd23aaf86ff4f1f54ef2259eb0a43289006f14
Yau Man Kit <Yau Man Kit>
2017-02-18 05:27:24 +0900

    Added header component Removed unused assets

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+    render(){
+        return <div className="Header">
+            <div>Logo</div>
+            <div>Home</div>
+            <div>Resume</div>
+            <div>Project</div>
+            <div>Hobby</div>
+        </div>
+    }
+}
+
+export default Header;
